1e9e4c0b798ac1473128a6d31a36a380cb01c3a8
Francisco Cisneros <franbichir@gmail.com>
2024-06-07 21:20:22 -0300

    agregado los test de los requests para ver todos los textos y buscar textos

--- app/models/text_history.py
+++ app/models/text_history.py
@@ -1,21 +1,17 @@
+# ------------------------------- importaciones ------------------------------ #
 from dataclasses import dataclass
 from app import db
 from typing import List
+
+# ----------------------------- fin importaciones ---------------------------- #
 
 
 @dataclass(init=False, repr=True, eq=True)
 class TextHistory(db.Model):
     __tablename__ = "text_histories"
+    # ------------------------------ columnas tabla ------------------------------ #
     id: int = db.Column(db.Integer, primary_key=True, autoincrement=True)
     text_id: int = db.Column(db.Integer, db.ForeignKey("texts.id"), nullable=False)
     content: str = db.Column(db.String(120), nullable=False)
     timestamp = db.Column(db.DateTime, default=db.func.current_timestamp())
-
-    @staticmethod
-    def get_versions_of_text(text_id: int) -> List["TextHistory"]:
-        # Obtiene todas las versiones de un texto específico.
-        return (
-            TextHistory.query.filter_by(text_id=text_id)
-            .order_by(TextHistory.timestamp.desc())
-            .all()
-        )
+    # ---------------------------- fin columnas tabla ---------------------------- #
---
+++ test/test_requests.py
@@ -0,0 +1,63 @@
+# ------------------------------- importaciones ------------------------------ #
+import unittest
+import requests
+from app.models import Text
+from app import create_app, db
+from app.services import EncryptService
+from app.repositories import TextRepository
+
+# ----------------------------- fin importaciones ---------------------------- #
+
+# ------------------------- servicios y repositorios ------------------------- #
+encrypt_service = EncryptService()
+text_repository = TextRepository()
+# ----------------------- fin servicios y repositorios ----------------------- #
+
+
+class TestRequests(unittest.TestCase):
+    def setUp(self):
+        # --------------------------- seteo atributos texto -------------------------- #
+        self.CONTENT_PRUEBA = "Hola mundo"
+        self.LENGTH_PRUEBA = len(self.CONTENT_PRUEBA)
+        self.LANGUAGE_PRUEBA = "es"
+        # ------------------------- fin seteo atributos texto ------------------------ #
+        self.app = create_app()
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+        db.create_all()
+
+    # * METODO PARA DESTRUIR LA BASE DE DATOS
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+        self.app_context.pop()
+
+    # * GETTER DE TEXTO
+    def __get_text(self):
+        text = Text()
+        text.content = self.CONTENT_PRUEBA
+        text.length = self.LENGTH_PRUEBA
+        text.language = self.LANGUAGE_PRUEBA
+        return text
+
+    def test_display_all_texts(self):
+        text = self.__get_text()
+        text_repository.save(text)
+        response = requests.get("http://127.0.0.1:5000/all_texts")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.headers["Content-Type"], "application/json")
+
+    def test_search_texts(self):
+        text = self.__get_text()
+        text_repository.save(text)
+        response = requests.get("http://127.0.0.1:5000/get_text/1")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.headers["Content-Type"], "application/json")
+
+
+if __name__ == "__main__":
+    unittest.main()
+
+
+if __name__ == "__main__":
+    unittest.main()
