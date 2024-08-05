d579a3c01e1833ebc45b9e69febb90b05abb4f63
Francisco Cisneros <franbichir@gmail.com>
2024-08-05 14:40:34 -0300

    cambios del profesor hasta el 19 de junio

--- app/resources/home.py
+++ app/resources/home.py
@@ -1,15 +1,21 @@
 # Importa las funciones jsonify y Blueprint desde el módulo flask
 from flask import jsonify, Blueprint
+from app.mapping.response_schema import ResponseSchema
+from app.services.response_message import ResponseBuilder
+
 
 # Define un blueprint llamado 'home'
-home = Blueprint('home', __name__)
+home = Blueprint("home", __name__)
+
+response_schema = ResponseSchema()
+
 
 # Define una ruta para el endpoint '/' (raíz del sitio) dentro del blueprint 'home'
-@home.route('/', methods=['GET'])
+@home.route("/", methods=["GET"])
 def index():
-    # Crea una respuesta JSON con el contenido "OK"
-    resp = jsonify("OK")
-    # Establece el código de estado de la respuesta como 200 (Éxito)
-    resp.status_code = 200
-    # Retorna la respuesta
-    return resp
+    response_builder = ResponseBuilder()
+    response_builder.add_message("Bienvenidos").add_status_code(200).add_data(
+        {"title": "API Auth"}
+    )
+    response = response_builder.build()
+    return response_schema.dump(response), 200
--- app/services/__init__.py
+++ app/services/__init__.py
@@ -4,3 +4,4 @@ from .role_services import RoleService
 from .profile_services import ProfileService
 from .encrypt_service import EncryptService
 from .text_service import TextService
+from .response_message import ResponseMessage
